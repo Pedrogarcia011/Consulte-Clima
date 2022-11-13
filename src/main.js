159d95885a6035452b0d7d7fbcce104311331f19
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-11-13 00:08:45 -0300

    Desenvolvendo lógica do projeto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 // 1f1941ef26300e3cddf7f63da8d05595 Key API Weather 
 import './style.css'
-const city = 'Brasília'
+const city = document.querySelector('#textCity');
+const cityResult = document.querySelector('#city');
+const button = document.querySelector('.button');
+const temperatura = document.querySelector('.temp');
+const clima = document.querySelector('#clima');
+const humidade = document.querySelector('#humidade');
+
+const trabalhandoClima = (dados) => {
+    console.log(dados);
+    temperatura.innerHTML += (dados.main.temp).toFixed(1);
+    cityResult.innerHTML = dados.name;
+    clima.innerHTML = dados.weather[0].description;
+}
 
 // const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`;
 const API_Clima = (city) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`)
   .then((resulve) => resulve.json())
+  .then((data) => trabalhandoClima(data));
 }
-console.log(API_Clima(city));
+
+button.addEventListener('click', () => {
+  API_Clima(city.value);
+})
+
+
