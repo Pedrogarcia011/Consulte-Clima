b31ae790ea59c334166cbff084d5ec2dccf2f0da
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-11-12 23:26:20 -0300

    Finalizando o estilo do projeto

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 // 1f1941ef26300e3cddf7f63da8d05595 Key API Weather 
+import './style.css'
 const city = 'Brasília'
 
-const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`;
+// const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`;
 const API_Clima = (city) => {
   return fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`)
   .then((resulve) => resulve.json())
 }
-console.log(API_Clima(city))
-console.log(api);
+console.log(API_Clima(city));
