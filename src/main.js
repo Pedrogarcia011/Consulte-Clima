4ad96f420186c87430570566f29cd6cdda2e733e
Pedrogarcia011 <pedrohenriquegracia011@gmail.com>
2022-11-12 21:37:01 -0300

    Initall

--- src/main.js
+++ src/main.js
@@ -1,1 +1,3 @@
 // 1f1941ef26300e3cddf7f63da8d05595 Key API Weather 
+
+const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=1f1941ef26300e3cddf7f63da8d05595&lang=pt_br`;
